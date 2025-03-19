478fb486dfb3026f4d2e8b829f1ede34b10d1deb
Maksym K <maxkr1203@gmail.com>
2025-03-19 13:11:17 +0200

    feat(31): calendar invites

--- src/modules/calendar/components/calendar-sidebar.tsx
+++ src/modules/calendar/components/calendar-sidebar.tsx
@@ -5,7 +5,7 @@ import { IoMdSearch } from 'react-icons/io';
 
 import { Button } from '@/shared/components/ui/button';
 import { Calendar } from '@/shared/components/ui/calendar';
-import { Dialog, DialogContent } from '@/shared/components/ui/dialog';
+import { Dialog, DialogContent, DialogTitle } from '@/shared/components/ui/dialog';
 import { Input } from '@/shared/components/ui/input';
 import {
   Sidebar,
@@ -76,6 +76,8 @@ export const CalendarSidebar = () => {
 
       <Dialog open={open} onOpenChange={setOpen}>
         <DialogContent>
+          <DialogTitle>Add calendar</DialogTitle>
+
           <CalendarForm action="add" onSubmit={() => setOpen(false)} />
         </DialogContent>
       </Dialog>
